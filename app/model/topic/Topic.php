1d9dc178d19ebb7d523a8d4a6038440a0b068128
Márton Balogh <37423520+Marcikadraga@users.noreply.github.com>
2022-08-10 12:56:41 +0200

    added topic modul function

--- app/model/topic/Topic.php
+++ app/model/topic/Topic.php
@@ -4,10 +4,214 @@ namespace app\model\topic;
 
 class Topic {
 
-    private $id = 0;
-    private $title='';
-    private $content='';
-    private $owner='';
+    private $id         = 0;
+    private $title      = '';
+    private $content    = '';
+    private $owner      = '';
+    private $created_at = '';
+    private $updated_at = '';
+    private $deleted_at = '';
 
 
+    public function __construct(?array $data = null) {
+
+        $this->fill($data);
+    }
+
+
+    public function fill(?array $data = null): void {
+
+        if (!is_null($data)) {
+            foreach ($this as $key => $value) {
+                if (array_key_exists($key, $data)) {
+                    $this->{$key} = $data[$key];
+                }
+            }
+        }
+    }
+
+
+    public function checkIsValidInsert(): bool {
+
+        $this->errors = [];
+
+        if (mb_strlen($this->title) < 3) {
+            $this->errors['title'] = 'A cím hossza legalább 3 karakter hosszúnak kell lennie!';
+        }
+        if (empty($this->title)) {
+            $this->errors['title'] = 'A cím megadása kötelező';
+        }
+        if (mb_strlen($this->content) < 3) {
+            $this->errors['content'] = 'A tartalom hossza legalább 3 karakter hosszúnak kell lennie!';
+        }
+        if (empty($this->title)) {
+            $this->errors['content'] = 'A tartalom megadása kötelező';
+        }
+        if (empty($this->owner)) {
+            $this->errors['owner'] = 'A tulajdonos megadása kötelező';
+        }
+        return (empty($this->errors));
+    }
+
+
+    public function checkIsValidUpdate(): bool {
+
+        $this->errors = [];
+
+        $this->checkIsValidInsert();
+
+        if (empty($this->id)) {
+            $this->errors['id'] = 'Hiba! Az entitás id-ja üres.';
+        }
+
+        return empty($this->errors);
+    }
+
+
+    public function checkIsValidSave(): bool {
+
+        if (empty($this->id)) {
+            return $this->checkIsValidInsert();
+        } else {
+            return $this->checkIsValidUpdate();
+        }
+    }
+
+
+    public function getErrors(): array {
+
+        return $this->errors;
+    }
+
+
+    public function getErrorsAsString($separator = '<br>'): string {
+
+        return implode($separator, $this->errors);
+    }
+
+
+    /**
+     * @return int
+     */
+    public function getId(): int {
+
+        return $this->id;
+    }
+
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void {
+
+        $this->id = $id;
+    }
+
+
+    /**
+     * @return string
+     */
+    public function getTitle(): string {
+
+        return $this->title;
+    }
+
+
+    /**
+     * @param string $title
+     */
+    public function setTitle(string $title): void {
+
+        $this->title = $title;
+    }
+
+
+    /**
+     * @return string
+     */
+    public function getContent(): string {
+
+        return $this->content;
+    }
+
+
+    /**
+     * @param string $content
+     */
+    public function setContent(string $content): void {
+
+        $this->content = $content;
+    }
+
+
+    /**
+     * @return string
+     */
+    public function getOwner(): string {
+
+        return $this->owner;
+    }
+
+
+    /**
+     * @param string $owner
+     */
+    public function setOwner(string $owner): void {
+
+        $this->owner = $owner;
+    }
+
+
+    /**
+     * @return string
+     */
+    public function getCreatedAt(): string {
+
+        return $this->created_at;
+    }
+
+
+    /**
+     * @param string $created_at
+     */
+    public function setCreatedAt(string $created_at): void {
+
+        $this->created_at = $created_at;
+    }
+
+
+    /**
+     * @return string
+     */
+    public function getUpdatedAt(): string {
+
+        return $this->updated_at;
+    }
+
+
+    /**
+     * @param string $updated_at
+     */
+    public function setUpdatedAt(string $updated_at): void {
+
+        $this->updated_at = $updated_at;
+    }
+
+
+    /**
+     * @return string
+     */
+    public function getDeletedAt(): string {
+
+        return $this->deleted_at;
+    }
+
+
+    /**
+     * @param string $deleted_at
+     */
+    public function setDeletedAt(string $deleted_at): void {
+
+        $this->deleted_at = $deleted_at;
+    }
 }
---
+++ app/model/topic/TopicModel.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace app\model\topic;
+
+use app\model\BaseModel;
+use Exception;
+use PDO;
+
+class TopicModel extends BaseModel{
+
+    function GetAllTopics(){
+
+        try {
+
+            $query='SELECT * FROM topics WHERE deleted_at IS NULL';
+            $topics=[];
+
+            $statement=$this->pdo->prepare($query);
+            $statement->execute();
+
+            $result=$statement->fetchAll(PDO::FETCH_ASSOC);
+
+            if(!empty($result)){
+                foreach ($result as $item){
+                    $topics[]=new Topic($item);
+                }
+            }
+            return $topics;
+
+        } catch (Exception $exception) {
+            die($exception->getMessage());
+        }
+    }
+
+
+
+
+}
